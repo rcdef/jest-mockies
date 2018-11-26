ea0cf65c6b7e99896c5a9d41ace9ab27132b4b8c
wengkhing <wengkhing@seekasia.com>
2018-11-27 07:57:11 +0800

    build: dist

---
+++ dist/dynamodb.js
@@ -0,0 +1,118 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _mockPut$mockBatchWri;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var mockBatchGet = jest.fn();
+var mockBatchWrite = jest.fn();
+var mockDelete = jest.fn();
+var mockGet = jest.fn();
+var mockPut = jest.fn();
+var mockQuery = jest.fn();
+var mockScan = jest.fn();
+var mockUpdate = jest.fn();
+
+var _default = (_mockPut$mockBatchWri = {
+  mockPut: mockPut,
+  mockBatchWrite: mockBatchWrite,
+  mockDelete: mockDelete,
+  mockGet: mockGet
+}, _defineProperty(_mockPut$mockBatchWri, "mockPut", mockPut), _defineProperty(_mockPut$mockBatchWri, "mockQuery", mockQuery), _defineProperty(_mockPut$mockBatchWri, "mockScan", mockScan), _defineProperty(_mockPut$mockBatchWri, "mockUpdate", mockUpdate), _defineProperty(_mockPut$mockBatchWri, "DynamoDB", {
+  DocumentClient:
+  /*#__PURE__*/
+  function () {
+    function DocumentClient() {
+      _classCallCheck(this, DocumentClient);
+    }
+
+    _createClass(DocumentClient, [{
+      key: "batchGet",
+      value: function batchGet(params) {
+        return {
+          promise: function promise() {
+            return mockBatchGet(params);
+          }
+        };
+      }
+    }, {
+      key: "batchWrite",
+      value: function batchWrite(params) {
+        return {
+          promise: function promise() {
+            return mockBatchWrite(params);
+          }
+        };
+      }
+    }, {
+      key: "delete",
+      value: function _delete(params) {
+        return {
+          promise: function promise() {
+            return mockDelete(params);
+          }
+        };
+      }
+    }, {
+      key: "get",
+      value: function get(params) {
+        return {
+          promise: function promise() {
+            return mockGet(params);
+          }
+        };
+      }
+    }, {
+      key: "put",
+      value: function put(params) {
+        return {
+          promise: function promise() {
+            return mockPut(params);
+          }
+        };
+      }
+    }, {
+      key: "query",
+      value: function query(params) {
+        return {
+          promise: function promise() {
+            return mockQuery(params);
+          }
+        };
+      }
+    }, {
+      key: "scan",
+      value: function scan(params) {
+        return {
+          promise: function promise() {
+            return mockScan(params);
+          }
+        };
+      }
+    }, {
+      key: "update",
+      value: function update(params) {
+        return {
+          promise: function promise() {
+            return mockUpdate(params);
+          }
+        };
+      }
+    }]);
+
+    return DocumentClient;
+  }()
+}), _mockPut$mockBatchWri);
+
+exports.default = _default;
