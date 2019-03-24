0c69f6d5f9a2278d06231d35340f7c5fffee4571
limyang51 <limyang51@gmail.com>
2019-03-24 16:30:07 +0800

    feature: mock s3

---
+++ src/aws-sdk/s3.js
@@ -0,0 +1,18 @@
+const mockGetObject = jest.fn();
+
+
+export default {
+  Mock: {
+    S3: {
+      getObject: mockGetObject
+    }
+  },
+
+  S3: class {
+    getObject(params) {
+      return {
+        promise: () => mockGetObject(params)
+      };
+    }
+  }
+};
