c849490af791e522a61b2e99c75c37b40cd3cccd
jinyung <huntthetux@gmail.com>
2019-05-01 14:37:13 +0800

    added ApiGatewayManagementApi

--- src/aws-sdk/aws-sdk-mock.js
+++ src/aws-sdk/aws-sdk-mock.js
@@ -2,21 +2,28 @@ import mockCognitoISP from './cognito-isp';
 import mockDynamoDB from './dynamodb';
 import mockSNS from './sns';
 import mockSQS from './sqs';
+import mockApiGatewayManagementApi from './api-gateway-management-api';
 
 const { Mock: mockCognitoISPFunctions, ...restMockCognitoISP } = mockCognitoISP;
 const { Mock: mockDynamoDBFunctions, ...restMockDynamoDB } = mockDynamoDB;
 const { Mock: mockSNSFunctions, ...restMockSNS } = mockSNS;
 const { Mock: mockSQSFunctions, ...restMockSQS } = mockSQS;
+const {
+  Mock: mockApiGatewayManagementApiFunctions,
+  ...restMockApiGatewayManagementApi
+} = mockApiGatewayManagementApi;
 
 export default {
   ...restMockCognitoISP,
   ...restMockDynamoDB,
   ...restMockSNS,
   ...restMockSQS,
+  ...restMockApiGatewayManagementApi,
   Mock: {
     ...mockCognitoISPFunctions,
     ...mockDynamoDBFunctions,
     ...mockSNSFunctions,
-    ...mockSQSFunctions
+    ...mockSQSFunctions,
+    ...mockApiGatewayManagementApiFunctions
   }
 };
